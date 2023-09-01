4023b6d5fece3dbcf5a2c4549aeee79ef88bdeed
Elmer Hernández <elmeralbertoperez@gmail.com>
2023-09-01 09:52:12 -0600

    Guia1
    Guia 1 Completada

---
+++ Guia 1/Control_Notas/src/main/java/com/mycompany/control_notas/Control_Notas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.control_notas;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+public class Control_Notas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Guia 1/LabClase1/src/main/java/com/mycompany/labclase1/LabClase1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labclase1;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+public class LabClase1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
